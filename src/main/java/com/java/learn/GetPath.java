eb40826e96e88333c386bbe7940d2ce4927e1c79
“weisong” <450428539@qq.com>
2020-01-17 16:00:58 +0800

    20200117

---
+++ src/main/java/com/java/learn/GetPath.java
@@ -0,0 +1,16 @@
+package com.java.learn;
+
+import org.apache.logging.log4j.core.config.plugins.util.ResolverUtil;
+
+/**
+ * description: 描述
+ * author: ws
+ * time: 2020/1/13 17:04
+ */
+
+public class GetPath {
+
+    public static void main(String[] args) {
+        System.out.println("user.dir=====" + System.getProperty("user.dir"));
+    }
+}
